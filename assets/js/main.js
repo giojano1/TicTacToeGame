b922ce1f149eb1844934a17bc253953a2b61b723
giojano1 <giorgi.janelidze64@gmail.com>
2024-06-05 14:22:48 +0400

    function

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,47 +4,83 @@ const sideXimg = document.querySelector(".xImg");
 const sideOimg = document.querySelector(".oImg");
 const menuCont = document.querySelector(".menu");
 const gameCont = document.querySelector(".game");
-const playCpuBtn = document.querySelector("#comp");
 const playMultiBtn = document.querySelector("#multi");
-const box = document.querySelectorAll(".box");
-let player1Side = "";
+const cells = document.querySelectorAll(".box");
+let currentPlayer = "X";
+let sideImg = "";
 // pick side from menu
 sideX.addEventListener("click", () => {
   sideO.classList.remove("active");
   sideX.classList.add("active");
   sideXimg.src = "assets/img/navy-x.svg";
   sideOimg.src = "assets/img/gray-o.svg";
-  player1Side = sideX.value;
-  console.log(player1Side);
+  currentPlayer = `${sideX.value}`;
 });
 sideO.addEventListener("click", () => {
   sideO.classList.add("active");
   sideX.classList.remove("active");
   sideXimg.src = "assets/img/gray-x.svg";
   sideOimg.src = "assets/img/navy-o.svg";
-  player1Side = sideO.value;
-  console.log(player1Side);
+  currentPlayer = `${sideO.value}`;
 });
 // play game
 function showGame() {
   menuCont.style.display = "none";
   gameCont.style.display = "flex";
 }
-// cpu
-playCpuBtn.addEventListener("click", () => {
-  showGame();
-  box.forEach((cont) => {
-    cont.addEventListener("click", () => {
-      if (cont.childElementCount === 0) {
-        const symbol = document.createElement("img");
-        symbol.src = "assets/img/icon-x.svg";
-        cont.appendChild(symbol);
-      }
-    });
-  });
-});
 
-// multiplayer
 playMultiBtn.addEventListener("click", () => {
   showGame();
 });
+const boardState = Array(9).fill(null);
+cells.forEach((cell) => {
+  cell.addEventListener("click", handleCellClick);
+});
+
+function handleCellClick(event) {
+  const cell = event.target;
+  const cellIndex = cell.getAttribute("data-index");
+
+  if (boardState[cellIndex] || checkWinner()) {
+    return;
+  }
+
+  boardState[cellIndex] = currentPlayer;
+  cell.textContent = "";
+
+  const img = document.createElement("img");
+  img.src =
+    currentPlayer === "X" ? "assets/img/icon-x.svg" : "assets/img/icon-o.svg";
+  img.alt = currentPlayer;
+
+  cell.appendChild(img);
+
+  if (checkWinner()) {
+    alert(`Player ${currentPlayer} wins!`);
+  } else if (boardState.every((cell) => cell)) {
+    alert("It's a draw!");
+  } else {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+  }
+}
+function checkWinner() {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // Rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // Columns
+    [0, 4, 8],
+    [2, 4, 6], // Diagonals
+  ];
+
+  return winningCombinations.some((combination) => {
+    const [a, b, c] = combination;
+    return (
+      boardState[a] &&
+      boardState[a] === boardState[b] &&
+      boardState[a] === boardState[c]
+    );
+  });
+}
